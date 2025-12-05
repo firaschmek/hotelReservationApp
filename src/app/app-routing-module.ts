7a724c8d809023796cf2c9214e97bbe99b1d083a
firas <firaschmek4@gmail.com>
2025-12-05 09:46:22 +0100

    creating routes

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Home } from './home/home';
+import { ReservationList } from './reservation-list/reservation-list';
+import { ReservationForm } from './reservation-form/reservation-form';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: Home },
+  { path: "lsit", component: ReservationList },
+  { path: "new", component: ReservationForm }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
